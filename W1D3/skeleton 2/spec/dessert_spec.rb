84a8b93a2ef3339693e3294b2422e78e21ecf2b4
emilycjones <emilycjones@college.harvard.edu>
2018-10-03 08:00:06 -0400

    still adding specs

--- W1D3/skeleton 2/spec/dessert_spec.rb
+++ W1D3/skeleton 2/spec/dessert_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require 'dessert'
+require 'dessert.rb'
 
 =begin
 Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
@@ -18,22 +18,32 @@ describe Dessert do
     end
 
     it "starts ingredients as an empty array" do
-      expect(desert.ingredients).to be ([])
+      expect(dessert.ingredients).to be_empty(Array)
     end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an 'argument error' when given a non-integer quantity" do
+      expect do
+        dessert.quantity('potato')
+      end.to raise_error("ArgumentError")
+    end
   end
+end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient('sugar')
+      expect(dessert.ingredients[-1]).to be ('sugar')
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      expect(dessert.mix).to eq(ingredients.shuffle)
+    end
   end
 
   describe "#eat" do
     it "subtracts an amount from the quantity"
+    expect
 
     it "raises an error if the amount is greater than the quantity"
   end
